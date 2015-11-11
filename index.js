bb5316eb429e09f09d6647f94216149071304afe
James Ide <ide@jameside.com>
2015-11-10 20:22:17 -0800

    [0.2.1] Fix require in the preset

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
   plugins: [
     require('babel-plugin-syntax-decorators'),
     require('babel-plugin-transform-decorators'),
-    require('babel-plugin-transform-es2015-function-name'),
+    require('babel-plugin-transform-es2015-block-scoped-functions'),
   ],
 };
